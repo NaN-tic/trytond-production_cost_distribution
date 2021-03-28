034fa53708c7c1a7e11daea275eec01590a2d3c2
Albert Cervera i Areny <albert@nan-tic.com>
2021-03-28 01:02:39 +0100

    Pyflakes fixes

--- tests/test_production_cost_distribution.py
+++ tests/test_production_cost_distribution.py
@@ -2,20 +2,13 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 import unittest
-from decimal import Decimal
-import doctest
 import trytond.tests.test_tryton
-from trytond.exceptions import UserError
-from trytond.pool import Pool
-from trytond.tests.test_tryton import ModuleTestCase, with_transaction
-from trytond.tests.test_tryton import doctest_teardown
-from trytond.tests.test_tryton import doctest_checker
+from trytond.tests.test_tryton import ModuleTestCase
 
 
 class TestCase(ModuleTestCase):
     'Test module'
     module = 'production_cost_distribution'
-
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
